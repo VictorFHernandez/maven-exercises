42ab6a7ff47d119973eca305888d4add8181b6d9
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-13 14:25:15 -0500

    promped the user to enter a string and return true if it was a string and false if it's not

--- src/main/mavenPractice.java
+++ src/main/mavenPractice.java
@@ -1,5 +1,16 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class mavenPractice {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String userInput = scanner.nextLine();
+        if(StringUtils.isNumeric(userInput)){
+            System.out.println(userInput + " is not a string...");
+        }else{
+            System.out.println(userInput + " is a String");
+        }
     }
 }

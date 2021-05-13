56d407f30e4fe7868c334dc5f368c915f88b9359
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-13 14:40:18 -0500

    added a .isEmpty from the StringsUtils

--- src/main/mavenPractice.java
+++ src/main/mavenPractice.java
@@ -9,11 +9,18 @@ public class mavenPractice {
         String userInput = scanner.nextLine();
         if(StringUtils.isNumeric(userInput)){
             System.out.println(userInput + " is not a string...");
+        }else if(StringUtils.isEmpty(userInput)){
+            System.out.println(" ");
         }else{
             System.out.println(userInput + " is a String");
         }
         System.out.println(StringUtils.swapCase(userInput));
         System.out.println(StringUtils.reverse(userInput));
         System.out.println(StringUtils.repeat(userInput + " ", 50));
+        if(StringUtils.isEmpty(userInput)){
+            System.out.println("BAKA!.. you did't write anything");
+        }else{
+            System.out.println("Thanks for writing a String, You're Awesome :)");
+        }
     }
 }
